b647c210a8fd6742392500e4ec9b20af5192158b
LeeJunHyun <ljhyun33@korea.ac.kr>
2019-08-01 03:15:30 +0900

    Fix bug

--- download_pdfs.py
+++ download_pdfs.py
@@ -34,6 +34,7 @@ for pid,j in db.items():
     if not os.path.exists(os.path.join(Config.pdf_dir,year)): os.makedirs(os.path.join(Config.pdf_dir,year))
     if not os.path.exists(os.path.join(Config.pdf_dir,year,month)): os.makedirs(os.path.join(Config.pdf_dir,year,month))
   else:
+    if not os.path.exists(os.path.join(Config.pdf_dir,year)): os.makedirs(os.path.join(Config.pdf_dir,year))
     year = basename.split('.')[0][:2]
     month = ""
 
